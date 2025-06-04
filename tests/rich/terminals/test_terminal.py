aaff02399e5f3b9e707517c618c6f637803a849f
Dani Vainstein <solmarkn@gmail.com>
2025-06-04 12:27:42 -0500

    fixed test to supprt isatty

--- tests/rich/terminals/test_terminal.py
+++ tests/rich/terminals/test_terminal.py
@@ -6,12 +6,14 @@
 from __future__ import annotations
 
 import itertools
+import os
 import sys
 from typing import TYPE_CHECKING, Generator, Callable, Any
 
 import pytest
 from hamcrest import assert_that, equal_to, none, is_not, any_of
 from pytest import param
+from rich.color import ColorSystem
 from rich.console import Console, WINDOWS
 from rich.pretty import Pretty
 from rich.panel import Panel
@@ -31,105 +33,153 @@ def callable_console() -> CallableConsole:
     yield _console
     return "Console create successfuly"
 
-
+'\x1b[1m[\x1b[0m\n    \x1b[3;91mfalse\x1b[0m,\n    \x1b[3;92mtrue\x1b[0m,\n    \x1b[3;35mnull\x1b[0m,\n    \x1b[32m"foo"\x1b[0m\n\x1b[1m]\x1b[0m\n'
 filepath = "C:/Users/solma/PycharmProjects/baloto-colombia/tests/rich/terminals/test_terminal.py"
 PRINT_TEXT = [
     "[bright_white text]A [i]text[/] [magenta]with[/] style[/]",
     f"[link={filepath}:{13}][bright_blue]click here to dive into the linked code.[/][/link]",
 ]
 
-@pytest.mark.parametrize("color_system", [None, "truecolor", "auto"])
+# -- ColorSystem
+TRUECOLOR = ColorSystem.TRUECOLOR
+WINDOWS = ColorSystem.WINDOWS
+EIGHT_BIT = ColorSystem.EIGHT_BIT
+STANDARD = ColorSystem.STANDARD
+
+@pytest.mark.parametrize("color_system", ["auto", "truecolor", "standard"])
 @pytest.mark.parametrize("force_terminal", [None, True, False])
 @pytest.mark.parametrize("legacy_windows", [None, True, False])
 def test_console_isatty_and_non_isatty(
-    request: pytest.FixtureRequest,
     record_property: Callable[[str, object], None],
     callable_console: CallableConsole, color_system: str | None, force_terminal: bool | None, legacy_windows: bool | None
 ):
+    """
+    https://gist.github.com/kurahaupo/6ce0eaefe5e730841f03cb82b061daa2
+
+    :param callable_console: Callable of Console
+    :param color_system: The color system to be tested: 'auto' (default), 'truecolor' or 'standard'
+    :param force_terminal: Enable/disable terminal control codes, or None to auto-detect terminal
+    :param legacy_windows:  Enable legacy Windows mode, or ``None`` to auto-detect.
+    """
     options = dict(color_system=color_system, force_terminal=force_terminal, legacy_windows=legacy_windows)
     console = callable_console(options)
-    isatty = sys.stdout.isatty()
-    options["isatty"] = isatty
-
+    isatty = console.file.isatty()
     console.line()
-    pretty = Pretty(options)
-    panel = Panel(pretty, title=f"isatty -> {isatty}", style="yellow")
-    console.print(panel)
-    for text in PRINT_TEXT:
-        console.print(text)
+    panel = Panel(Pretty(options), title=f"isatty -> {isatty}", style="yellow")
+    console.print(panel, new_line_start=True)
+    console.print("", *PRINT_TEXT, sep="\n")
 
-    if isatty:
-        record_property(f"ISATTY CONFIG", "color_system to None will never show colors or links")
-        record_property(f"ISATTY CONFIG", "color_system='truecolor'")
-        record_property(f"ISATTY CONFIG", "color_system='auto' should be used with force_terminal=True|None")
-        record_property(f"ISATTY CONFIG", "legacy-windows=true -> deactivates the links")
-        record_property(f"ISATTY RECOMMENDATION", "color_system='truecolor', legacy_windows=None|False")
-    else:
-        record_property("NON-ISATTY COMMENT", "true-color with legacy_windows=False -> links looking bad.")
-    try:
-        if force_terminal:
-            assert_that(console.is_terminal, reason="is_terminal")
-        elif force_terminal is False:
-            assert_that(not console.is_terminal, reason="is_terminal")
-        elif isatty:
-            assert_that(console.is_terminal, reason="is_terminal")
-        else:
-            assert_that(not console.is_terminal, reason="is_terminal")
-    except AssertionError as e:
-        print(e)
+    record_property("ISATTY MODE", isatty)
 
-    if color_system is None:
+    match color_system:
+        case "truecolor":
+            # -- force_terminal → NONE
+            if force_terminal is None and legacy_windows is None:
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is None and legacy_windows is False:
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor, not legacy")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is None and legacy_windows:
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor, legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
 
-        if force_terminal is None and legacy_windows is None:
-            assert_that(console.color_system, none(), reason="default console, no keywords")
-        elif force_terminal is None and legacy_windows is False:
-            assert_that(console.color_system, none(), reason="not legacy_win")
-        elif force_terminal is None and legacy_windows:
-            assert_that(console.color_system, none(), reason="legacy_win")
-        elif force_terminal is False and legacy_windows is None:
-            assert_that(console.color_system, none(), reason="not force_term")
-        elif force_terminal is False and legacy_windows is False:
-            assert_that(console.color_system, none(), reason="not force_term, not legacy_win")
-        elif force_terminal is False and legacy_windows:
-            assert_that(console.color_system, none(), reason="not force_term, legacy_win")
-        elif force_terminal and legacy_windows is None:
-            assert_that(console.color_system, none(), reason="force_term")
-        elif force_terminal and legacy_windows is False:
-            assert_that(console.color_system, none(), reason="force_term, not legacy_win")
-        elif force_terminal and legacy_windows:
-            assert_that(console.color_system, none(), reason="force_term, legacy_win")
-        else:
-            pytest.skip("Unsupported combination")
+            # -- force_terminal → FALSE    any_of("truecolor", none())
+            elif force_terminal is False and legacy_windows is None:
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor, not force_term")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is False and legacy_windows is False:
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor, not force_term, not legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is False and legacy_windows:
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor, not force_term, legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
 
-    elif color_system in ["truecolor", "auto"]:
-        if force_terminal is None and legacy_windows is None:
-            assert_that(console.color_system, any_of("truecolor", none()), reason="truecolor|auto")
-        elif force_terminal is None and legacy_windows is False:
-            assert_that(console.color_system, any_of("truecolor", none()), reason="truecolor, not legacy")
-        elif force_terminal is None and legacy_windows:
-
-            if WINDOWS and color_system == "auto" and isatty:
-                assert_that(console.color_system, any_of(none(), "windows"), reason="auto, legacy_win")
+            # -- force_terminal → TRUE
+            elif force_terminal and legacy_windows is None:
+                assert_that(console.is_terminal, reason="is_terminal")
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor, force_term")
+            elif force_terminal and legacy_windows is False:
+                assert_that(console.is_terminal, reason="is_terminal")
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor|auto, force_term, not legacy")
+            elif force_terminal and legacy_windows:
+                assert_that(console.is_terminal, reason="is_terminal")
+                assert_that(console._color_system, equal_to(TRUECOLOR), reason="truecolor, force_term, legacy_win")
             else:
-                assert_that(console.color_system, any_of("truecolor", none(), "windows"), reason="truecolor|auto, legacy_win")
-        elif force_terminal is False and legacy_windows is None:
-            assert_that(console.color_system, any_of("truecolor", none()), reason="truecolor|auto, not force_term")
-        elif force_terminal is False and legacy_windows is False:
-            assert_that(console.color_system, any_of("truecolor", none()), reason="truecolor|auto, not force_term, not legacy_win")
-        elif force_terminal is False and legacy_windows:
-            assert_that(console.color_system, any_of("truecolor", none()), reason="truecolor|auto, not force_term, legacy_win")
-        elif force_terminal and legacy_windows is None:
-            if WINDOWS and color_system == "auto"and isatty is False:
-                assert_that(console.color_system, equal_to("windows"), reason="auto, force_term")
+                pytest.skip("Unsupported combination")
+
+        case "standard":
+            # -- force_terminal → NONE
+            if force_terminal is None and legacy_windows is None:
+                assert_that(not console.is_terminal, reason="is_terminal")
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard")
+            elif force_terminal is None and legacy_windows is False:
+                assert_that(not console.is_terminal, reason="is_terminal")
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, not legacy")
+            elif force_terminal is None and legacy_windows:
+                assert_that(not console.is_terminal, reason="is_terminal")
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, legacy_win")
+
+            # -- force_terminal → FALSE
+            elif force_terminal is False and legacy_windows is None:
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, not force_term")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is False and legacy_windows is False:
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, not force_term, not legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is False and legacy_windows:
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, not force_term, legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
+
+            # -- force_terminal → TRUE
+            elif force_terminal and legacy_windows is None:
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, force_term")
+                assert_that(console.is_terminal, reason="is_terminal")
+            elif force_terminal and legacy_windows is False:
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, force_term, not legacy")
+                assert_that(console.is_terminal, reason="is_terminal")
+            elif force_terminal and legacy_windows:
+                assert_that(console._color_system, equal_to(STANDARD), reason="standard, force_term, legacy_win")
+                assert_that(console.is_terminal, reason="is_terminal")
             else:
-                assert_that(console.color_system, equal_to("truecolor"), reason="truecolor, force_term")
-        elif force_terminal and legacy_windows is False:
-            record_property("NON-ISATTY COMMENT", "link look a mess on truecolor on non-isatty")
-            assert_that(console.color_system,  any_of("truecolor", "256"), reason="truecolor|auto, force_term, not legacy")
-        elif force_terminal and legacy_windows:
-            if WINDOWS and color_system == "auto":
-                assert_that(console.color_system, equal_to("windows"), reason="auto, force_term, legacy_win")
+                pytest.skip("Unsupported combination")
+
+        case "auto":
+            # -- force_terminal → NONE
+            if force_terminal is None and legacy_windows is None:
+                is_equal_none = equal_to(TRUECOLOR) if isatty else none()
+                assert_that(console._color_system, is_equal_none, reason=f"auto, issaty={isatty}")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is None and legacy_windows is False:
+                assert_that(console._color_system, none(), reason="auto, not legacy")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is None and legacy_windows:
+                assert_that(console._color_system, none(), reason="auto, legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
+
+            # -- force_terminal → FALSE
+            elif force_terminal is False and legacy_windows is None:
+                assert_that(console._color_system, none(), reason="auto, not force_term")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is False and legacy_windows is False:
+                assert_that(console._color_system, none(), reason="auto, not force_term, not legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
+            elif force_terminal is False and legacy_windows:
+                assert_that(console._color_system, none(), reason="auto, not force_term, legacy_win")
+                assert_that(not console.is_terminal, reason="is_terminal")
+
+            # -- force_terminal → TRUE
+            elif force_terminal and legacy_windows is None:
+                assert_that(console._color_system, equal_to(WINDOWS), reason="auto, force_term")
+                assert_that(console.is_terminal, reason="is_terminal")
+            elif force_terminal and legacy_windows is False:
+                is_equal = equal_to(TRUECOLOR) if isatty else equal_to(EIGHT_BIT)
+                assert_that(console._color_system, is_equal, reason=f"auto, force_term, not legacy issaty={isatty}")
+                assert_that(console.is_terminal, reason="is_terminal")
+            elif force_terminal and legacy_windows:
+                assert_that(console._color_system, equal_to(WINDOWS), reason="auto, force_term, legacy_win")
+                assert_that(console.is_terminal, reason="is_terminal")
             else:
-                assert_that(console.color_system, equal_to("truecolor"), reason="truecolor, force_term, legacy_win")
-        else:
-            pytest.skip("Unsupported combination")
+                pytest.skip("Unsupported combination")
+        case _:
+            pytest.skip("Not implemented system color: only ['auto', 'standard', 'truecolor']")
